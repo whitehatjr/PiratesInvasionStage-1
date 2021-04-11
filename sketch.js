ca24a5815744ddff20157e16c9096ed7eb756334
whitehatjr <whitehatjr@whitehatjr.com>
2021-04-12 02:12:05 +0530

    creating tower, ground and cannon

---
+++ sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+var canvas, angle, tower, ground, cannon;
+
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth - 200, windowHeight - 150);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(width / 2 - 650, height - 290, 250, 580);
+  cannon = new Cannon(width / 2 - 600, height / 2 - 220, 120, 40, angle);
+  cannonBall = new CannonBall(cannon.x, cannon.y);
+
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  
+
+  Engine.update(engine);
+  ground.display();
+  
+
+  cannon.display();
+  tower.display();
+  cannonBall.display()
+ 
+}
+
+
+
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) {
+    cannonBall.shoot()
+  }
+}
